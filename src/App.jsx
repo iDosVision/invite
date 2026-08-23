c64e067bf0bbb0825ec649d20c1b59a8c0a60f01
Aidos Akhmetzhan <aidosakhmetzhan@gmail.com>
2026-08-23 20:38:03 +0200

    router fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import PanamaItinerary from './components/PanamaItinerary';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/travel" element={<PanamaItinerary />} />
       </Routes>
-    </BrowserRouter >
+    </HashRouter >
   );
 }
 
